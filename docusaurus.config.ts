7f2a3c841bf76f979e397342a7b82aa8af39e6a5
thaamaraikannan <thaamaraikannans.sst@gmail.com>
2026-06-16 11:01:59 +0530

    chore: update deployment configuration for GitHub Pages hosting

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,15 +11,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://adavoudi.info',
+  url: 'https://sst-cloud-solutions.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/aws-study-library/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'adavoudi', // Usually your GitHub org/user name.
-  projectName: 'aws-sap', // Usually your repo name.
+  organizationName: 'sst-cloud-solutions', // Usually your GitHub org/user name.
+  projectName: 'aws-study-library', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
